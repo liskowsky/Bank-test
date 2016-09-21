4de72b18c3078ba4d0c66b23ba06d4baa7de192f
liskowsky <karollebiedzinski@gmail.com>
2016-09-21 14:55:28 +0100

    Second version

--- lib/account.rb
+++ lib/account.rb
@@ -1,27 +1,38 @@
 class Account
 
-  attr_reader :balance, :statement
+  attr_reader :current_balance, :statement
 
   def initialize
-    @balance = 0
+    @current_balance = 0
     @statement = []
   end
 
   def deposit(deposit_amount)
-    @balance += deposit_amount
-    @statement << { date: Time.new.strftime("%d/%m/%Y"), credit: deposit_amount, balance: @balance}
+    @current_balance += deposit_amount
+    add_transaction("credit", deposit_amount)
   end
 
   def withdrawal(withdrawal_amount)
-    @balance -= withdrawal_amount
-    @statement << { date: Time.new.strftime("%d/%m/%Y"), debit: withdrawal_amount, balance: @balance}
+    @current_balance -= withdrawal_amount
+    add_transaction("debit", withdrawal_amount)
   end
 
   def print_statement
     p "date || credit || deposit || balance"
+    @statement.reverse!
     @statement.each do |record|
-      p " #{record[:date]} || #{record[:credit]} || #{record[:debit]} || #{record[:balance]} "
+      p "#{record[:date]} || #{record[:credit]} || #{record[:debit]} || #{record[:balance]}"
     end
+  end
+
+  private
+
+  def todays_date
+    Time.new.strftime("%d/%m/%Y")
+  end
+
+  def add_transaction(tansaction_name, transaction_amount)
+    @statement << { date: todays_date, "#{tansaction_name.to_sym}": transaction_amount, balance: @current_balance }
   end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,7 +4,7 @@ describe Account do
   describe "when new account is created" do
     it "has a balance of 0 by default" do
       account = Account.new
-      expect(account.balance).to eq 0
+      expect(account.current_balance).to eq 0
     end
   end
 
@@ -12,11 +12,11 @@ describe Account do
     account = Account.new
 
     it "can be topped up" do
-      expect{ account.deposit(500) }.to change{ account.balance }.by 500
+      expect{ account.deposit(500) }.to change{ account.current_balance }.by 500
     end
 
     it "can have a withdrawal" do
-      expect{ account.withdrawal(500) }.to change{ account.balance }.by -500
+      expect{ account.withdrawal(500) }.to change{ account.current_balance }.by -500
     end
   end
 
@@ -33,7 +33,7 @@ describe Account do
       account.deposit 500
       it "shows the date when deposit was made" do
         time = Time.new.strftime("%d/%m/%Y")
-        expect(account.statement[0][:date].strftime("%d/%m/%Y")).to eq time
+        expect(account.statement[0][:date]).to eq time
       end
     end
 
